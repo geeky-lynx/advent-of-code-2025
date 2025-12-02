91fabb095a9626fe45ba23d66ed1e906aae1a0f3
geeky-lynx <markomacura003@gmail.com>
2025-12-02 23:42:29 +0100

    slowly fixing the segfault (could not believe setting up stack was the fix)

--- day01/run.c
+++ day01/run.c
@@ -9,11 +9,10 @@ extern int get_passwd();
 extern int read_from_file(char* p_direction, int* p_offset);
 
 extern int read_from_file(char* p_direction, int* p_offset) {
-  printf("%p %p\n", &c, &i);
-  printf("%p %p\n", p_direction, p_offset);
-  printf("%c %d\n", *p_direction, *p_offset);
-  int r = fscanf(s_file, "%c%d\n", &c, &i);
-  printf("read done\n");
+  static int rc = 0;
+  int r = fscanf(s_file, "%c%d\n", &c, &i); // but not dis
+  printf("fscanf ret val = %d\n", r);
+  printf("#%d: read done (with c = %c, i = %d)\n", rc++, c, i);
   *p_direction = c;
   *p_offset = i;
   return r;
@@ -26,7 +25,7 @@ int main() {
     return -1;
   }
   int result = get_passwd();
-  printf("The password is: %d\n", result);
+  printf("The password is: %d (%x)\n", result, result);
   fclose(s_file);
   return 0;
 }
