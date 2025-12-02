20ce6a49d8d3b8216f752ebe4fdcfbbae6093e55
geeky-lynx <markomacura003@gmail.com>
2025-12-02 21:48:46 +0100

    segfaults man

--- day01/compile.sh
+++ day01/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Compile with GCC
 # gcc -c -o solution.o solution.S -Wall -Wextra -g
-gcc -o run solution.S run.c -Wall -Wextra -g
+gcc -o run solution.S run.c -Wall -Wextra -g -O0
--- day01/run.c
+++ day01/run.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+extern int read_from_file(FILE* p_file, char* p_direction, int* p_offset);
+
+extern int read_from_file(FILE* p_file, char* p_direction, int* p_offset) {
+  char dir = 0;
+  int offs = 0;
+  int res = fscanf(p_file, "%c%d\n", &dir, &offs);
+  *p_direction = dir;
+  *p_offset = offs;
+  return res;
+}
+
 extern int get_passwd();
 
 int main() {
