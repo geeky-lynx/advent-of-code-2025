eda5d3a96ceb7688815cc9df5f968d290f9d0e53
geeky-lynx <markomacura003@gmail.com>
2025-12-02 22:37:27 +0100

    weird segfault...

--- day01/run.c
+++ day01/run.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-extern int read_from_file(FILE* p_file, char* p_direction, int* p_offset);
-
-extern int read_from_file(FILE* p_file, char* p_direction, int* p_offset) {
-  char dir = 0;
-  int offs = 0;
-  int res = fscanf(p_file, "%c%d\n", &dir, &offs);
-  *p_direction = dir;
-  *p_offset = offs;
-  return res;
-}
+static FILE* s_file;
+static char c;
+static int i;
 
 extern int get_passwd();
+extern int read_from_file(char* p_direction, int* p_offset);
+
+extern int read_from_file(char* p_direction, int* p_offset) {
+  printf("%p %p\n", &c, &i);
+  printf("%p %p\n", p_direction, p_offset);
+  printf("%c %d\n", *p_direction, *p_offset);
+  int r = fscanf(s_file, "%c%d\n", &c, &i);
+  printf("read done\n");
+  *p_direction = c;
+  *p_offset = i;
+  return r;
+}
 
 int main() {
+  s_file = fopen("input.txt", "r");
+  if (s_file == NULL) {
+    printf("Error opening a file\n");
+    return -1;
+  }
   int result = get_passwd();
   printf("The password is: %d\n", result);
+  fclose(s_file);
   return 0;
 }
