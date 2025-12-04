0fdcbd215cfed2402e97cc6ebe628c9e6a06a237
geeky-lynx <markomacura003@gmail.com>
2025-12-04 22:40:53 +0100

    remove temporary debug-print stuff

--- day01/run.c
+++ day01/run.c
@@ -2,20 +2,12 @@
 #include <stdlib.h>
 
 static FILE* s_file;
-static char c;
-static int i;
 
 extern int get_passwd();
 extern int read_from_file(char* p_direction, int* p_offset);
 
 extern int read_from_file(char* p_direction, int* p_offset) {
-  static int rc = 0;
-  int r = fscanf(s_file, "%c%d\n", &c, &i); // but not dis
-  printf("fscanf ret val = %d\n", r);
-  printf("#%d: read done (with c = %c, i = %d)\n", rc++, c, i);
-  *p_direction = c;
-  *p_offset = i;
-  return r;
+  return fscanf(s_file, "%c%d\n", p_direction, p_offset);
 }
 
 int main(int argc, char **argv) {
